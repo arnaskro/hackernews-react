e8a38548ee76793225413a804fc0b9fed4319f27
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-07-30 16:54:01 +0200

    App & Spinner file

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store, { history } from './reducers/'
+import css from './App.scss';
+import { BrowserRouter, Route, Link }  from 'react-router-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="container">
+                    <br />
+                    <div className="text-center">
+                        <Link to={'/'} className="btn btn-link">Home</Link>
+                    </div><br />
+                    <Route exact path="/" render={(props) => {
+                        return (
+                            <div className="text-center">
+                                <h1>App is ready!</h1>
+                            </div>
+                        )
+                    }} />
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
