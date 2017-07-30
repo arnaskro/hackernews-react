ffa291eefa768d47bad30c15fe4b2096bda7db9f
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-07-30 17:12:40 +0200

    API urls

--- src/api.js
+++ src/api.js
@@ -1,1 +1,17 @@
 export const API_URL = "https://hacker-news.firebaseio.com/v0/";
+
+const API_URL_BUILDER = (path) => { return API_URL + path + ".json" };
+
+// Stories
+export const TOP_STORIES = () => { return API_URL_BUILDER("topstories") };
+export const NEW_STORIES = () => { return API_URL_BUILDER("newstories") };
+export const BEST_STORIES = () => { return API_URL_BUILDER("beststories") };
+export const ASK_STORIES = () => { return API_URL_BUILDER("askstories") };
+export const SHOW_STORIES = () => { return API_URL_BUILDER("showstories") };
+export const JOB_STORIES = () => { return API_URL_BUILDER("jobstories") };
+
+// Items
+export const GET_ITEM = (itemId) => { return API_URL_BUILDER("item/" + itemId) };
+
+// Users
+export const GET_USER = (user) => { return API_URL_BUILDER("user/" + user) };
