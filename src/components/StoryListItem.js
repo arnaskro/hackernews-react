dc4706740c0d69c4db943b7a4af108b208ddc709
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-07-30 20:08:12 +0200

    story list item meta

--- src/components/StoryListItem.js
+++ src/components/StoryListItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 export default class StoryListItem extends React.Component {
 
     componentWillMount() {
         this.props = this.props.story;
+    }
+
+    itemLink() {
+        return "www.google.com";
     }
 
     getTitle() {
@@ -13,11 +18,34 @@ export default class StoryListItem extends React.Component {
             return (this.props.title);
     }
 
+    getMeta() {
+        //71 points by kbp 4 hours ago | hide | 3 comments
+        return (
+            <div>
+                <span>{this.props.score}</span> points by <span>{this.props.by}</span> <span><Moment unix fromNow>{this.props.time}</Moment></span> {this.getCommentsMeta()}
+            </div>
+        );
+    }
+
+    getCommentsMeta() {
+         if (this.props.type == "story")
+            if (this.props.descendants > 0)
+                return  (
+                    <span><a href={this.itemLink()}>{this.props.descendants} comments</a></span>
+                );
+            else 
+                return  (
+                    <span><a href={this.itemLink()}>discuss</a></span>
+                );
+    }
+
     render() {
         
         return (
             <li>
                 {this.getTitle()}
+                <br />
+                {this.getMeta()}
             </li>
         )
     }
