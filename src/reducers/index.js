0ca008f5ec7b89f46d8e4a9f454c316afe567929
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-07-30 17:50:35 +0200

    general reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
+import TopStoryReducer from './TopStoryReducer';
 import thunk from 'redux-thunk';
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+    topStories: TopStoryReducer
+});
 
 const logger = createLogger();
 const middleware = applyMiddleware(thunk, logger);
